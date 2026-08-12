0aee2357a35528ef7a8e0da910596b597f2a120b
M Awais Faryad <awaisfaryad25@gmail.com>
2026-08-12 16:59:58 +0500

    button fixing

--- src/app/(public)/components/global/PageHero.tsx
+++ src/app/(public)/components/global/PageHero.tsx
@@ -54,10 +54,10 @@ const PageHero: React.FC<PageHeroProps> = ({
 
       <div className="absolute inset-0 flex items-center justify-start">
         <div className="mx-auto w-full max-w-7xl px-6 md:px-0">
-          <span className=" inline-flex rounded-full border border-primary/20! bg-white/20 backdrop-blur-md px-4 py-2 text-xs 4xl:text-[13px] text-white shadow-md font-semibold uppercase tracking-widest">
-            {eyebrow}
-          </span>
           <div className="flex flex-col items-start gap-4">
+            <span className=" inline-flex rounded-full border border-primary/20! bg-white/20 backdrop-blur-md px-4 py-2 text-xs 4xl:text-[13px] text-white shadow-md font-semibold uppercase tracking-widest">
+              {eyebrow}
+            </span>
             <h1 className="font-display text-3xl md:text-4xl lg:text-5xl 2xl:text-6xl 4xl:text-7xl font-bold leading-[1.2] md:leading-[1.3] text-white! capitalize">
               {title}
             </h1>
@@ -68,12 +68,12 @@ const PageHero: React.FC<PageHeroProps> = ({
             <div className="flex flex-wrap items-center gap-4 md:gap-5 pt-4">
               <Link
                 href={href}
-                className="group flex items-center gap-2 font-semibold border border-white hover:bg-white px-5 py-3 rounded-lg relative cursor-pointer overflow-hidden"
+                className="group flex items-center gap-2 font-semibold border border-white hover:border-primary hover:bg-linear-to-r from-primary to-secondary px-5 py-3 rounded-lg relative cursor-pointer overflow-hidden"
               >
-                <span className="relative z-10 text-white group-hover:text-transparent group-hover:bg-clip-text group-hover:bg-linear-to-r group-hover:from-brand-blue group-hover:to-brand-green transition-all duration-300">
+                <span className="relative z-10 text-white transition-all duration-300">
                   {btnTitle}
                 </span>
-                <span className="relative z-10 size-6 flex items-center justify-center rounded-full transition-all duration-300 group-hover:bg-linear-to-r group-hover:from-brand-blue group-hover:to-brand-green">
+                <span className="relative z-10 size-6 flex items-center justify-center rounded-full transition-all duration-300 group-hover:bg-linear-to-r group-hover:from-primary group-hover:to-secondary">
                   <ArrowUpRight className="size-3 text-white group-hover:text-white transition-all duration-300 group-hover:rotate-45" />
                 </span>
               </Link>
